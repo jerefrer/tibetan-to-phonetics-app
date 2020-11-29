6ae539c1c963ce5cd8da8016e302c7fae6da05ab
Jérémy FRERE <frere.jeremy@gmail.com>
2020-11-29 17:57:47 +0100

    Adds HHDL & Karmapa long-life prayers with new exceptions

---
+++ tests/texts/hhdl-shabkar.js
@@ -0,0 +1,11 @@
+testGroups.push({
+  name: 'HHDL long-life prayer by Shabkar',
+  includeInPercentage: true,
+  sentences: true,
+  tests: [
+    { tibetan: '༈ སྟོང་ཉིད་སྙིང་རྗེ་ཟུང་དུ་འཇུག་པའི་ལམ། །', transliteration: "tong-nyi nyingjé sungtu jukpé lam" },
+    { tibetan: 'ཆེས་ཆེར་གསལ་མཛད་གངས་ཅན་བསྟན་འགྲོའི་མགོན། །', transliteration: "chécher saldzé kangchen tendrö gön" },
+    { tibetan: 'ཕྱག་ན་པདྨོ་བསྟན་འཛིན་རྒྱ་མཚོ་ལ། །', transliteration: "chakna padmo tendzin gyamtso la" },
+    { tibetan: 'གསོལ་བ་འདེབས་སོ་བཞེད་དོན་ལྷུན་གྲུབ་ཤོག།', transliteration: "sölwa depso zhétön lhüntrup sho" },
+  ]
+});
---
+++ tests/texts/karmapa.js
@@ -0,0 +1,13 @@
+testGroups.push({
+  name: 'Karmapa long-life prayer',
+  includeInPercentage: true,
+  sentences: true,
+  tests: [
+    { tibetan: 'འཆི་མེད་རྩ་གསུམ་འདུས་ཞལ་ཨོ་རྒྱན་རྗེའི། །', transliteration: "chimé tsasum düzhal orgyen jé" },
+    { tibetan: 'ཐུགས་རྗེ་འགྲོ་འདུལ་ཕྲིན་ལས་གར་བསྒྱུར་བ། །', transliteration: "thukjé drodül tr'inlé kargyur wa" },
+    { tibetan: 'རྡེ་རྗེ་འཆང་དབང་སྐྱབས་མགོན་ཀརྨ་པའི། །', transliteration: "déjé changwang kyapgön karma pé" },
+    { tibetan: 'ཞབས་པད་བརྟན་ཅིང་མཛད་ཕྲིན་རྒྱས་གྱུར་ཅིག །།', transliteration: "zhapé tenching dzétr'in gyékyur chik" },
+  ]
+});
+
+
