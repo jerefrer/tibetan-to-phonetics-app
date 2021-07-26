b7ec4628e310c618946051cb4d3eabfc2eabd407
Jérémy FRERE <frere.jeremy@gmail.com>
2021-07-26 19:24:06 +0200

    Adds more tests for Sanskrit stuff

--- tests/exceptions/ram-yam-kham-and-friends.js
+++ tests/exceptions/ram-yam-kham-and-friends.js
@@ -1,9 +1,23 @@
 testGroups.push({
   name: 'Exceptions - Ram Yam Kham and friends',
   tests: [
+    { tibetan: 'རཾ་', transliteration: 'ram' },
+    { tibetan: 'རྃ་', transliteration: 'ram' },
+    { tibetan: 'རྂ་', transliteration: 'ram' },
+    { tibetan: 'ཡྃ་', transliteration: 'yam' },
+    { tibetan: 'ཁྂ་', transliteration: 'kham' },
+
+    { tibetan: 'ཧཱུཾ', transliteration: 'hung' },
+    { tibetan: 'ཧཱུྃ', transliteration: 'hung' },
+    { tibetan: 'ཧཱུྂ', transliteration: 'hung' },
+
     { tibetan: 'རཾ་ཡཾ་ཁཾ་', transliteration: 'ram yam kham' },
     { tibetan: 'རྃ་ཡྃ་ཁྃ་', transliteration: 'ram yam kham' },
     { tibetan: 'རྂ་ཡྂ་ཁྂ་', transliteration: 'ram yam kham' },
+    { tibetan: 'ༀ་ཨཱཿཧཱུྃ', transliteration: 'om ah hung' },
+    { tibetan: 'ཧ་ཧོ་ཧྲཱི' , transliteration: 'ha ho hri' },
+
+    { tibetan: 'ༀ་སརྦ་བྷུ་ཏ་ཨ་ཀར་ཁ་ཡ་ཛ' , transliteration: 'om sarva bhuta akar shaya dja' },
 
     { tibetan: 'ཝཾ', transliteration: 'wam' },
     { tibetan: 'བཾ', transliteration: 'bam' },
