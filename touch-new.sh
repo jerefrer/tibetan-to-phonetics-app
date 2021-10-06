06fcbaac170d9c047248e21d253d2d676636e431
Jérémy FRERE <frere.jeremy@gmail.com>
2021-10-06 08:53:39 +0200

    Updates touch-new.sh script to handle renamed and untracked files

--- touch-new.sh
+++ touch-new.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for file in `git st | grep "\.js" | grep -v "\-dist" | sed s/.*modified:\ *//`
+for file in `git st | grep "\.js" | grep -v "\-dist" | sed s/.*modified:\ *// | sed "s/.*renamed:.* -> //" | sed "s/\s*//" | uniq`
 do
   touch $file
   sleep 1
