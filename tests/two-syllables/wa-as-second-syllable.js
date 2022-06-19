cd2ca8be076bc1135456677b539dce373659cf24
Jérémy FRERE <frere.jeremy@gmail.com>
2022-06-19 15:07:06 +0200

    Adds tests for BaAsWa according to 'Gilbert Buéso - Parlons Tibétain'

--- tests/two-syllables/wa-as-second-syllable.js
+++ tests/two-syllables/wa-as-second-syllable.js
@@ -1,6 +1,7 @@
 testGroups.push({
-  name: 'Wa as second syllable',
+  name: 'Wa as second syllable - First syllable ending with ra should yield Wa',
   tests: [
+    { tibetan: 'འཕིར་བ་',  transliteration: "p'irwa" },
     { tibetan: 'བར་བ་',  transliteration: 'parwa' },
     { tibetan: 'བར་བར་',  transliteration: 'parwar' },
     { tibetan: 'བར་བའོ་',  transliteration: 'parwa-o' },
@@ -11,5 +12,45 @@ testGroups.push({
     { tibetan: 'བར་བའི་', transliteration: 'parwé' },
     { tibetan: 'བར་བའམ་', transliteration: 'parwa-am' },
     { tibetan: 'བར་བའང་', transliteration: 'parwa-ang' },
+  ]
+})
+
+testGroups.push({
+  name: 'Wa as second syllable - First syllable ending with nga should yield Wa',
+  tests: [
+    { tibetan: 'གནང་བ་',  transliteration: 'nangwa' },
+  ]
+})
+
+
+testGroups.push({
+  name: "Wa as second syllable - First syllable ending with 'a should yield Wa",
+  tests: [
+    { tibetan: 'དཀའ་བ་',  transliteration: 'kawa' },
+  ]
+})
+
+
+testGroups.push({
+  name: 'Wa as second syllable - First syllable ending with la should yield Wa',
+  tests: [
+    { tibetan: 'མཇལ་བ་',  transliteration: 'jelwa' },
+  ]
+})
+
+
+testGroups.push({
+  name: 'Wa as second syllable - First syllable ending with no suffix should yield Wa',
+  tests: [
+    { tibetan: 'འགྲོ་བ་',  transliteration: 'drowa' },
+  ]
+})
+
+// For now we don't cover this one because it actually generates
+// more inaccurate transliterations in other weird cases
+testGroups.push({
+  name: 'Wa as second syllable - First syllable ending with anything else should yield Pa',
+  tests: [
+    { tibetan: 'བྱེད་བ་',  transliteration: 'chépa' },
   ]
 })
